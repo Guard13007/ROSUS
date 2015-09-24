3ff13eca3b354309895cc6d4c12dad0aa8fb0659
Paul Liverman III <paul.liverman.iii@gmail.com>
2015-09-23 17:46:15 -0700

    improved Ubify - removed trailing whitespace from output - increased readability of source

--- src/Ubify.cpp
+++ src/Ubify.cpp
@@ -12,42 +12,56 @@ string ubify(string baseString);
 bool matches(char c);
 
 int main(int argc, char *argv[]) {
+
     if (argc >= 2) {
-        for (int i = 1; i < argc; i++) {
+        for (int i = 1; i < argc-1; i++) {
             cout << ubify(argv[i]).c_str() << " ";
         }
-        cout << endl;
+
+        cout << ubify(argv[argc-1]) << endl;
+
     } else {
         string line = "";
+
         cout << "> " << flush;
         getline(cin, line);
+
         if (!line.empty()) {
             cout << ubify(line) << endl;
         }
     }
+
     return 0;
 }
 
 string ubify(string baseString) {
     string out = "";
-    int size = baseString.size();
-    for (int i = 0; i < size; i++) {
+    unsigned int size = baseString.size();
+
+    for (unsigned int i = 0; i < size; i++) {
         char ch = baseString[i];
+
         if (matches(ch)) {
-            if (isupper(ch))
+            if (isupper(ch)) {
                 out += "Ub";
-            else
+            } else {
                 out += "ub";
-        } else
+            }
+        } else {
             out += ch;
+        }
     }
+
     return out;
 }
 
 bool matches(char c) {
-    for (int i = 0; i < sizeof(replacementChar); i++) {
-        if (tolower(c) == replacementChar[i])
+
+    for (unsigned int i = 0; i < sizeof(replacementChar); i++) {
+        if (tolower(c) == replacementChar[i]) {
             return true;
+        }
     }
+
     return false;
 }
